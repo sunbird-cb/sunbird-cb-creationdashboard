a08ff88f0b34defc78531ecbf345f49577a1c45d
Amit Sengar <aAmitSengar@users.noreply.github.com>
2021-07-15 14:06:20 +0530

    Dev (#7)
    * jenkinsfile test commit
    
    * variable test
    
    * shell variable test
    
    * test commit
    
    * unwanted curly braces removed
    
    * shell command test
    
    * text file command added globally
    
    * build.sh used for docker build
    
    * testing of jenkins variable in the build.sh
    
    * commented lines are removed
    
    * email notification added in deployment script
    
    * updated docker repo variable
    
    * update: dashboard data update
    
    * update: dashboard data update
    
    * update: dashboard data
    
    * Update Dockerfile.dev
    
    * RAIN Library Integration v0.1.4 (#3)
    
    * update: dashboard data update
    
    * update: dashboard data update
    
    * update: dashboard data
    
    * feat: RAIN package upgrade
    
    Co-authored-by: Sakthivel <sakthivel.govindan@tarento.com>
    
    * Update Dockerfile.preprod
    
    * RAIN Integrations v0.1.5 (#4)
    
    * update: dashboard data update
    
    * update: dashboard data update
    
    * update: dashboard data
    
    * feat: RAIN package upgrade
    
    * feat: RAIN library v0.1.5 integrations and its data update
    
    * feat: RAIN library v0.1.5 integrations and its data update
    
    Co-authored-by: Sakthivel <sakthivel.govindan@tarento.com>
    
    * Dev server (#5)
    
    * new assest dir
    
    * Jenkinsfile-sun updated
    
    * Jenkinsfile-sun updated
    
    * Jenkinsfile-sun updated
    
    * Jenkinsfile-sun updated
    
    * Update server.js
    
    * Jenkinsfile-sun updated
    
    * test added Readme
    
    * Env Update with window Object
    
    * Env Update with window Object:lintFix
    
    * env Update for 1 image
    
    Co-authored-by: haridas.kakunje <haridas.kakunje@tarento.com>
    Co-authored-by: Haritest <haridasksd@gmail.com>
    
    * Dev server (#6)
    
    * jenkinsfile test commit
    
    * variable test
    
    * shell variable test
    
    * test commit
    
    * unwanted curly braces removed
    
    * shell command test
    
    * text file command added globally
    
    * build.sh used for docker build
    
    * testing of jenkins variable in the build.sh
    
    * commented lines are removed
    
    * new assest dir
    
    * Jenkinsfile-sun updated
    
    * Jenkinsfile-sun updated
    
    * Jenkinsfile-sun updated
    
    * Jenkinsfile-sun updated
    
    * Update server.js
    
    * Jenkinsfile-sun updated
    
    * email notification added in deployment script
    
    * updated docker repo variable
    
    * test added Readme
    
    * update: dashboard data update
    
    * update: dashboard data update
    
    * update: dashboard data
    
    * Update Dockerfile.dev
    
    * RAIN Library Integration v0.1.4 (#3)
    
    * update: dashboard data update
    
    * update: dashboard data update
    
    * update: dashboard data
    
    * feat: RAIN package upgrade
    
    Co-authored-by: Sakthivel <sakthivel.govindan@tarento.com>
    
    * Env Update with window Object
    
    * Env Update with window Object:lintFix
    
    * Update Dockerfile.preprod
    
    * RAIN Integrations v0.1.5 (#4)
    
    * update: dashboard data update
    
    * update: dashboard data update
    
    * update: dashboard data
    
    * feat: RAIN package upgrade
    
    * feat: RAIN library v0.1.5 integrations and its data update
    
    * feat: RAIN library v0.1.5 integrations and its data update
    
    Co-authored-by: Sakthivel <sakthivel.govindan@tarento.com>
    
    * env Update for 1 image
    
    * unwanted files updated to support new env
    
    Co-authored-by: gohilamariappan <41056032+gohilamariappan@users.noreply.github.com>
    Co-authored-by: haridas.kakunje <haridas.kakunje@tarento.com>
    Co-authored-by: Haritest <haridasksd@gmail.com>
    Co-authored-by: Sakthivel <sakthivel.govindan@tarento.com>
    Co-authored-by: Sakthivel G <sakthivel.g@outlook.in>
    
    Co-authored-by: gohilamariappan <41056032+gohilamariappan@users.noreply.github.com>
    Co-authored-by: Sakthivel <sakthivel.govindan@tarento.com>
    Co-authored-by: Haritest <haridasksd@gmail.com>
    Co-authored-by: Sakthivel G <sakthivel.g@outlook.in>
    Co-authored-by: haridas.kakunje <haridas.kakunje@tarento.com>

--- src/environments/environment.np.ts
+++ src/environments/environment.np.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  sitePath: 'cbc.karmayogiprod.nic.in',
-  karmYogiPath: 'https://karmayogiprod.nic.in',
-  cbpPath: 'https://cbp.karmayogiprod.nic.in',
+  sitePath: (window as { [key: string]: any })['env']['apiUrl'] || '', // 'cbc_3000',
+  karmYogiPath: (window as { [key: string]: any })['env']['apiUrl'] || '',
+  cbpPath: (window as { [key: string]: any })['env']['apiUrl'] || '',
 
 }
