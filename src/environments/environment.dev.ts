5335e829a81454c47df1a4926573b9e1a61bf752
Amit Kumar <amit.sengar@tarento.com>
2021-07-15 13:37:19 +0530

    env Update for 1 image

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  sitePath: (window as { [key: string]: any })['env']['apiUrl'] || 'default', // 'cbc_3000',
+  sitePath: (window as { [key: string]: any })['env']['apiUrl'] || '', // 'cbc_3000',
   karmYogiPath: (window as { [key: string]: any })['env']['apiUrl'] || '',
   cbpPath: (window as { [key: string]: any })['env']['apiUrl'] || '',
 }
