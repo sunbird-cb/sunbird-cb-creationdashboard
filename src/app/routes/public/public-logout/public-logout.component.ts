c6b7e9b41c648ea4186fe161303c625aa4cb469e
Amit Sengar <amitsengar@yahoo.com>
2022-05-13 13:39:51 +0530

    Logout landing Page #1

---
+++ src/app/routes/public/public-logout/public-logout.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { AuthKeycloakService, ConfigurationsService, NsPage } from '@sunbird-cb/utils'
+import { Subscription } from 'rxjs'
+import { ActivatedRoute } from '@angular/router'
+
+@Component({
+  selector: 'ws-public-logout',
+  templateUrl: './public-logout.component.html',
+  styleUrls: ['./public-logout.component.scss'],
+})
+export class PublicLogoutComponent implements OnInit, OnDestroy {
+  contactUsMail = ''
+  contactPage: any
+  platform = 'Learner'
+  panelOpenState = false
+  pageNavbar: Partial<NsPage.INavBackground> = this.configSvc.pageNavBar
+  private subscriptionContact: Subscription | null = null
+
+  constructor(
+    private configSvc: ConfigurationsService,
+    private activateRoute: ActivatedRoute,
+    private authSvc: AuthKeycloakService,
+  ) { }
+
+  ngOnInit() {
+    this.subscriptionContact = this.activateRoute.data.subscribe(data => {
+      this.contactPage = data.pageData.data
+    })
+    if (this.configSvc.instanceConfig) {
+      this.contactUsMail = this.configSvc.instanceConfig.mailIds.contactUs
+    }
+    this.authSvc.force_logout().then(() => { })
+  }
+
+  ngOnDestroy() {
+    if (this.subscriptionContact) {
+      this.subscriptionContact.unsubscribe()
+    }
+  }
+}
